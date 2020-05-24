ff60a81d55590993a20926ab5ec0542e7a814032
Jorge Luis Pérez de la Torre <jlpdelat@me.com>
2020-05-24 11:01:00 -0500

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -4,18 +4,17 @@ Plot3 <- function(){
    # I use one process to convert to prepare a dataset that is common for 
    # all the ploting work with Plot1 ~ Plot4 R Programs
    
-   ## plot Global_activep_ower <-> DateTime
+   ## plot Global_active_Power <-> DateTime
 
-   png("plot3.png", width=480, height=480)
+   png("plot3.png", width = 480, height = 480)
    
-   with( SubSource, plot( DateTime, Sub_metering_1, type="l", 
-                         xlab="Day", ylab="Energy sub metering"))
+   with( SubSource, plot( DateTime, Sub_metering_1, type = "l", xlab = "Day", ylab = "Energy sub metering"))
    
-   lines(SubSource$DateTime, SubSource$Sub_metering_2,type="l", col= "red")
-   lines(SubSource$DateTime, SubSource$Sub_metering_3,type="l", col= "blue")
+   lines(SubSource$DateTime, SubSource$Sub_metering_2,type = "l", col = "red")
+   lines(SubSource$DateTime, SubSource$Sub_metering_3,type = "l", col = "blue")
    
    legend(c("topright"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-          lty= 1, lwd=2, col = c("black", "red", "blue"))
+          lty = 1, lwd = 2, col = c("black", "red", "blue"))
    
    dev.off()
    
