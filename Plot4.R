be2fbcac18bf2d35a3227a3790b83a891eb1917e
Jorge Luis Pérez de la Torre <jlpdelat@me.com>
2020-05-24 11:12:27 -0500

    Minor cosmetic fixes

--- Plot4.R
+++ Plot4.R
@@ -6,23 +6,21 @@ Plot4 <- function() {
 
    ## Plot as required
    
-   png("plot4.png", width=480, height=480)
+   png("plot4.png", width = 480, height = 480)
    
-   par(mfrow=c(2,2))
+   par(mfrow = c(2,2))
    
-   with(SubSource, plot(DateTime, Global_active_power, type="l", xlab="", 
-                     ylab="Global Active Power"))
-   with(SubSource, plot(DateTime, Voltage, type = "l", xlab="datetime", ylab="Voltage"))
-   with(SubSource, plot(DateTime, Sub_metering_1, type="l", xlab="", 
-                     ylab="Energy sub metering"))
+   with(SubSource, plot(DateTime, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power"))
+   with(SubSource, plot(DateTime, Voltage, type = "l", xlab = "DateTime", ylab = "Voltage"))
+   with(SubSource, plot(DateTime, Sub_metering_1, type="l", xlab = "", ylab = "Energy sub metering"))
 
-   lines(SubSource$DateTime, SubSource$Sub_metering_2,type="l", col= "red")
-   lines(SubSource$DateTime, SubSource$Sub_metering_3,type="l", col= "blue")
+   lines(SubSource$DateTime, SubSource$Sub_metering_2,type = "l", col = "red")
+   lines(SubSource$DateTime, SubSource$Sub_metering_3,type = "l", col = "blue")
 
-   legend("topright", lty=1, col=c("black","red","blue"),
-          legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex = 0.6)
+   legend("topright", lty = 1, col = c("black","red","blue"),
+          legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.6)
 
-   with(SubSource, plot(DateTime, Global_reactive_power, type="l", xlab="datetime", 
-                     ylab="Global_reactive_power"))
+   with(SubSource, plot(DateTime, Global_reactive_power, type = "l", xlab = "DateTime", 
+                     ylab = "Global_reactive_power"))
    dev.off()
 }
